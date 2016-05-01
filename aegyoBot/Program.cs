446b36763a054025b9225f2ff639b6b38f504e90
Matt <metchutran@gmail.com>
2016-05-01 14:39:27 -0700

    change startup command to "dnx start"

--- aegyoBot/Program.cs
+++ aegyoBot/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord.Modules;
+using Discord.Commands;
+using Discord.Audio;
+using Discord;
 
 namespace aegyoBot
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) => new Program().Start(args);
+
+        private const string AppName = "aegyoBot";
+        private const string AppUrl = "https://github.com/senahf/aegyoBot";
+
+        public static DiscordClient _client;
+
+        private void Start(string[] args)
         {
-			Console.WriteLine("Hello from DNX!");
-            Console.WriteLine("Press enter to quit.");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
