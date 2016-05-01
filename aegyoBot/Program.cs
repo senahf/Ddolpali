6b6beb8c3b663e2e23851ad08158cdad1115a36d
Matt <metchutran@gmail.com>
2016-05-01 14:02:26 -0700

    start

---
+++ aegyoBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace aegyoBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Hello from DNX!");
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
